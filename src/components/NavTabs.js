af5b78dbe387231c092e914a0d7949da1e0b1c79
Algpym26 <110873247+Algpym26@users.noreply.github.com>
2023-01-15 23:21:36 -0500

    Navtabs

---
+++ src/components/NavTabs.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavTabs () {
+    return (
+        <div>
+            <h1>NavBar</h1>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Home</li>
+                <li>Portfolio</li>
+
+            </ul>
+        </div>
+    )
+}
+export default NavTabs 
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import Navtabs from "./NavTabs"
+
+
+
+
+
+
+
+function portfolioContainer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
